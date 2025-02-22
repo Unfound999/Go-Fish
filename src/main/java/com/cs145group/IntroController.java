399f0c77ed09a3bc5085113c6fee51ae0e62527d
Christopher Waschke <chrisdwaschke@gmail.com>
2025-02-21 21:10:08 -0800

    Added Controller for Intro View.

--- src/main/java/com/cs145group/IntroController.java
+++ src/main/java/com/cs145group/IntroController.java
@@ -1,4 +1,14 @@
 package com.cs145group;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
+
 public class IntroController {
+
+    @FXML
+    private void handlePlay() throws IOException {
+        App.setScene("game.fxml");
+    }
 }
