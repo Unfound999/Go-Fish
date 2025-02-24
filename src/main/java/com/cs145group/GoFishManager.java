93ec86bdc93c5f2f206f9e6739d0c510e5325e11
Brody Weinkauf <brody.weinkauf@gmail.com>
2025-02-24 13:59:59 -0800

    Added dealCards method (The new file "Player.java" is a temporary test file for testing the methods in GoFishManager.java)

--- src/main/java/com/cs145group/GoFishManager.java
+++ src/main/java/com/cs145group/GoFishManager.java
@@ -1,9 +1,8 @@
 package com.cs145group;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
-
-//  temporary import as Chris's Stack file is not in this build
 import java.util.Stack;
 
 
@@ -15,6 +14,8 @@ public class GoFishManager {
         //  initializes suit enum variable
         Suit suit = Suit.DIAMONDS;
 
+        //  Temporary line for testing purposes (ensure that the deck is being created correctly)
+        System.out.println("Created Deck");
         //  first for loop determines suit the cards while nested for loop determines rank
         for (int i = 0; 4 > i; i++) {
             switch(i) {
@@ -43,13 +44,42 @@ public class GoFishManager {
         Collections.shuffle(deck, new Random());
 
         // Temporary line for testing purposes (ensure that the order of cards in deck are being randomized)
-        System.out.printf("%d%s%s", deck.peek().getRank(), " of ", deck.peek().getSuit());
+        System.out.println("Shuffled deck top card");
+        System.out.printf("%d%s%s%n%n", deck.peek().getRank(), " of ", deck.peek().getSuit());
     } //  end of shuffleDeck method
+
+    public static void dealCards(Stack<CardTest> deck, ArrayList<CardTest> userHand, ArrayList<CardTest> cpuHand) {
+        for (int i = 0; 5 > i; i++) {
+            userHand.add(deck.pop());
+            cpuHand.add(deck.pop());
+        } //  end of for loop
+    } //  end of dealCards method
 
     //  main method for testing purposes
     public static void main(String[] args) {
+
         Stack<CardTest> deck = new Stack<CardTest>();
+        ArrayList<CardTest> userHand = new ArrayList<>();
+        ArrayList<CardTest> cpuHand = new ArrayList<>();
+
+        int userScore = 0;
+        int cpuScore = 0;
+
+        Player user = new Player(userHand, userScore);
+        Player cpu = new Player(cpuHand, cpuScore);
+
         createDeck(deck);
         shuffleDeck(deck);
+        dealCards(deck, userHand, cpuHand);
+
+        // temporary for loops for testing purposes
+        System.out.println("Dealt User Hand");
+        for (int i = 0; userHand.size() > i; i++) {
+            System.out.printf("%d%s%s%n", userHand.get(i).getRank(), " of ", userHand.get(i).getSuit());
+        }
+        System.out.println("\nDealt cpu hand");
+        for (int i = 0; cpuHand.size() > i; i++) {
+            System.out.printf("%d%s%s%n", cpuHand.get(i).getRank(), " of ", cpuHand.get(i).getSuit());
+        }
     } //  ends main method
 } // ends GoFishManager class
