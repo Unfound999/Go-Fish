0357a0f4bdf945e21b44365f95f0273d5e1e2897
Unfound999 <jacksocrod@gmail.com>
2025-02-24 23:11:16 -0800

    Created Card Data

--- src/main/java/com/cs145group/Card.java
+++ src/main/java/com/cs145group/Card.java
@@ -2,6 +2,18 @@ package com.cs145group;
 
 public class Card {
 
+    private int rank;
+    private cardType suit;
+
+    public int getRank() { return this.rank; }
+    public cardType suit() { return this.suit; }
+
+    public Card(cardType suit, int rank) {
+
+        this.suit = suit;
+        this.rank = rank;
+    }
+
 }
 
 enum cardType {
