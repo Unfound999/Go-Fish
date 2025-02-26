16c5dee0f4eb8bce37118ccc29849a24da56854f
Christopher Waschke <chrisdwaschke@gmail.com>
2025-02-25 23:41:50 -0800

    Added Score tracking, fixed stack location bugs, switched playBook to check card object equality, with new .equals method.

--- src/main/java/com/cs145group/GoFishManager.java
+++ src/main/java/com/cs145group/GoFishManager.java
@@ -10,11 +10,28 @@ public class GoFishManager {
     private Stack<Card> deck;
     private ArrayList<Card> userHand;
 
+    private int cpuScore = 0;
+    private int userScore = 0;
+
+    public String checkWinState(){
+        if(this.userHand.size() == 0 || this.CPUHand.size() == 0){
+            if(this.cpuScore < this.userScore){
+                return "win";
+            }
+            return "lost";
+        }
+        return "playing"; // This value really isn't used.
+    }
+
     public ArrayList<Card> getUserHand() {
         return userHand;
     }
 
     private ArrayList<Card> CPUHand;
+
+    public int getCPUHandSize(){
+        return this.CPUHand.size();
+    }
     
     public GoFishManager(){
         this.deck = new Stack<Card>(Card.class, 52);
@@ -48,24 +65,19 @@ public class GoFishManager {
             } //  end of switch case
             for (int j = 0; 13 > j; j++) {
                 this.deck.push(new Card(suit, j+2));
-
-                //  Temporary lines for testing purposes (ensure that the deck is being created correctly)
-                System.out.printf("%d%s%s%n", this.deck.peek().getRank(), " of ", this.deck.peek().getSuit());
             } //  ends nested for loop
         } //  ends for loop
         System.out.println();
     } //  ends createDeck method
 
     public void shuffleDeck() {
-
         this.deck.shuffle();
-
-        // Temporary line for testing purposes (ensure that the order of cards in deck are being randomized)
-        System.out.println("Shuffled deck top card");
-        System.out.printf("%d%s%s%n%n", this.deck.peek().getRank(), " of ", this.deck.peek().getSuit());
     } //  end of shuffleDeck method
 
     public void dealCards() {
+        if(this.deck.getCurrentLocation() == 0){
+            return; // No more cards left to deal.
+        }
         for (int i = 0; 5 > i; i++) {
             this.userHand.add(this.deck.pop());
             this.CPUHand.add(this.deck.pop());
@@ -74,10 +86,17 @@ public class GoFishManager {
 
     public void userPlayHand(Card card) {
         askCard(card, this.userHand, this.CPUHand);
+        if(playBook(this.userHand)){
+            userScore++;
+        };
     } //  end of userPlayHand method
 
     public void cpuPlayHand(Card card) {
         askCard(card, this.CPUHand, this.userHand);
+        if(playBook(this.CPUHand)){
+            cpuScore++;
+        }
+        playBook(this.CPUHand);
     } //  end of userPlayHand method
 
     public void askCard(Card card, ArrayList<Card> grabberHand, ArrayList<Card> grabbeeHand) {
@@ -107,24 +126,29 @@ public class GoFishManager {
     } //  end of askCard method
 
     public void recieveCard(ArrayList<Card> hand) {
+        if(this.deck.getCurrentLocation() == 0){
+            return; // No more cards to deal.
+        }
         hand.add(this.deck.pop());
     } //  end of recieveCard method
 
     //Generate number size of hand
     //Call playtest method at the index of the random number
     public void cpuTurn() {
-
+        if(CPUHand.size() == 0){
+            return;
+        }
         Random randomNum = new SecureRandom();
         int randomCPU = randomNum.nextInt(this.CPUHand.size());
 
         cpuPlayHand(this.CPUHand.get(randomCPU));
     }
 
-    public void playBook(ArrayList<Card> hand) {
+    public boolean playBook(ArrayList<Card> hand) {
 
         for(int i = 0; i < hand.size(); i++) {
 
-            int firstCard = hand.get(i).getRank();
+            Card firstCard = hand.get(i);
 
             for(int j = 0; j < hand.size(); j++) {
 
@@ -132,8 +156,8 @@ public class GoFishManager {
 
                     continue;
                 }
-                int secondCard = hand.get(j).getRank();
-                if(firstCard == secondCard) {
+                Card secondCard = hand.get(j);
+                if(firstCard.equals(secondCard)) {
 
                     for(int k = 0; k < hand.size(); k++) {
 
@@ -142,17 +166,18 @@ public class GoFishManager {
                             continue;
                         }
 
-                        int thirdCard = hand.get(k).getRank();
+                        Card thirdCard = hand.get(k);
 
-                        if(thirdCard == secondCard && secondCard == firstCard) {
-
-                            hand.remove(i);
-                            hand.remove(j);
-                            hand.remove(k);
+                        if(thirdCard.equals(firstCard) && thirdCard.equals(secondCard)) {
+                            hand.remove(firstCard);
+                            hand.remove(secondCard);
+                            hand.remove(thirdCard);
+                            return true;
                         }
                     }
                 }
             }
         }
+        return false;
     }
 } // ends GoFishManager class
